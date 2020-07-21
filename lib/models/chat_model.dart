406b05ddb30aef6a6a06232621b9c6a7bd44bb9a
Gopinath-tech <65074941+Gopinath-tech@users.noreply.github.com>
2020-07-21 16:12:06 +0530

    Whatsapp_clone

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,59 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+    name: "Gopinath",
+    message: "Hey guys I am Gopinath",
+    time: "15:30",
+    avatarUrl: "https://wallpapercave.com/wp/wp3987280.jpg",
+  ),
+  new ChatModel(
+    name: "Giridaran",
+    message: "Hey guys I am Giridaran",
+    time: "17:30",
+    avatarUrl:
+        "https://i.pinimg.com/564x/81/9f/b8/819fb8b2baa84564043bfb1d81e7c323.jpg",
+  ),
+  new ChatModel(
+    name: "Selva Kumar",
+    message: "Hey guys I am Selva Kumar",
+    time: "18:30",
+    avatarUrl:
+        "https://i.pinimg.com/236x/9c/13/0f/9c130f4a650c7f7c799a898d101c474c.jpg",
+  ),
+  new ChatModel(
+    name: "Giridharaan",
+    message: "Hey guys I am Giridharaan",
+    time: "19:30",
+    avatarUrl:
+        "https://www.southdreamz.com/wp-content/uploads/2012/05/GV-Prakash-kumar.jpg",
+  ),
+  new ChatModel(
+    name: "Sudharshan",
+    message: "Hey guys I am Sudharshan",
+    time: "20:30",
+    avatarUrl:
+        "https://i.pinimg.com/236x/43/6c/a8/436ca86cdef69a836fdea034f8075ee3.jpg",
+  ),
+  new ChatModel(
+    name: "Janarthanan",
+    message: "Hey guys I am Janarthanan",
+    time: "21:30",
+    avatarUrl:
+        "https://cdn.pixabay.com/photo/2017/11/20/20/15/moon-2966579_1280.png",
+  ),
+  new ChatModel(
+    name: "Karthik Narayan",
+    message: "Hey guys I am Karthik Narayan",
+    time: "22:30",
+    avatarUrl:
+        "https://1.bp.blogspot.com/-5vj3sWmccCg/Xkgq_t9QMnI/AAAAAAAALSo/Npi7IXporL4aHMHfQS7C196l7DX34rUdgCNcBGAsYHQ/s1600/photo_38%254010-02-2019_23-49-50.jpg",
+  ),
+];
---
+++ lib/pages/camera_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/whatsapp_home.dart';
+
+class CameraScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Text(
+        "Camera",
+        style: new TextStyle(fontSize: 20.0),
+      ),
+    );
+  }
+}
